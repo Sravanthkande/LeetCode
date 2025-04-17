fcbba44903fd4c7200bd7dce0da67fa342b09f02
Sravanth Kumar Yadav <sravanthkt.21@gmail.com>
2025-04-17 10:10:15 +0530

    Update 0001-two-sum.py

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -5,12 +5,13 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        seen = {}
+        seen = {} #stores array elements in hashmap
         for i in range(len(nums)):
-            diff = target - nums[i]
-            if diff in seen:
+            diff = target - nums[i] #stores the difference of nums[i] and target
+            if diff in seen: #check the difference in map if found return i and map[diff]
                 return [i, seen[diff]]
-            seen[nums[i]] = i
-        return []
+            seen[nums[i]] = i  # if not update nums[i] value as i in map
+        return [] #return none
+
 
         
