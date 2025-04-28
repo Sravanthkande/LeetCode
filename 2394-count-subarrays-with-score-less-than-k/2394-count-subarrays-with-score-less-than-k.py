d264e2bd352d5bf7f5645ace46df9276ceb56d59
Sravanth Kumar Yadav <sravanthkt.21@gmail.com>
2025-04-28 10:14:45 +0530

    Time: 112 ms (81.41%), Space: 30.6 MB (42.08%) - LeetHub

--- 2394-count-subarrays-with-score-less-than-k/2394-count-subarrays-with-score-less-than-k.py
+++ 2394-count-subarrays-with-score-less-than-k/2394-count-subarrays-with-score-less-than-k.py
@@ -5,7 +5,7 @@ class Solution:
         i = 0
         for j in range(N):
             total += nums[j]
-            while  i <= j and total * (j-i+1) >= k:
+            while i <= j and total * (j-i+1) >= k:
                 total -= nums[i]
                 i += 1
             res += j - i + 1
