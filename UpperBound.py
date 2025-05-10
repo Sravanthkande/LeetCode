1a23430671ac512e13209dac908d549bf9f347c7
Sravanth Kumar Yadav <sravanthkt.21@gmail.com>
2025-05-10 10:16:45 +0530

    Add files via upload
    This is the code for upper bound

---
+++ UpperBound.py
@@ -0,0 +1,14 @@
+class Solution:
+    def upperBound(self, nums, x):
+        N = len(nums)
+        left, right = 0, N - 1
+        ans = N 
+
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] > x:
+                ans = mid 
+                right = mid - 1
+            else:
+                left = mid + 1
+        return ans
