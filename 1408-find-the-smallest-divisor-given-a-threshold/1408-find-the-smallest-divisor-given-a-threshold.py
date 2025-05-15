d796731b595094afdb56bc4d950bf4e52c39da6a
Sravanth Kumar Yadav <sravanthkt.21@gmail.com>
2025-05-15 05:45:36 +0530

    Time: 127 ms (58.01%), Space: 22.4 MB (91.47%) - LeetHub

---
+++ 1408-find-the-smallest-divisor-given-a-threshold/1408-find-the-smallest-divisor-given-a-threshold.py
@@ -0,0 +1,22 @@
+import math
+
+class Solution:
+    def getSum(self, nums, d):
+        sum = 0
+        for num in nums:
+            sum += math.ceil(num / d)
+        return sum
+
+    def smallestDivisor(self, nums: List[int], threshold: int) -> int:
+        N = len(nums)
+        if N > threshold:
+            return -1
+        
+        left, right =1, max(nums)
+        while left <= right:
+            mid = (left + right) // 2
+            if self.getSum(nums, mid) <= threshold:
+                right = mid - 1
+            else:
+                left = mid + 1
+        return left
