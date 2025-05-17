35b8addf66a04376a993f10249fe72fbf86bedae
Sravanth Kumar Yadav <sravanthkt.21@gmail.com>
2025-05-17 10:37:42 +0530

    Time: 3 ms (61.1%), Space: 17.8 MB (75.3%) - LeetHub

--- 1297-maximum-number-of-balloons/1297-maximum-number-of-balloons.py
+++ 1297-maximum-number-of-balloons/1297-maximum-number-of-balloons.py
@@ -1,6 +1,7 @@
-from collections import Counter
 class Solution:
     def maxNumberOfBalloons(self, text: str) -> int:
-        freq = Counter(text)
-
-        return min(freq.get('b', 0), freq.get('a', 0), freq.get('l',0) // 2, freq.get('o', 0)//2, freq.get('n', 0))
+        freq = {}
+        for char in text:
+            freq[char] = freq.get(char, 0) + 1
+        
+        return min(freq.get('b', 0), freq.get('a', 0), freq.get('l', 0) // 2, freq.get('o', 0) // 2, freq.get('n', 0))
