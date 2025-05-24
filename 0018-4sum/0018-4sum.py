5eef5245af080acc96ca56202703f8de1e1a57d0
Sravanth Kumar Yadav <sravanthkt.21@gmail.com>
2025-05-24 05:30:19 +0530

    Time: 511 ms (66.26%), Space: 12.4 MB (88.79%) - LeetHub

---
+++ 0018-4sum/0018-4sum.py
@@ -0,0 +1,38 @@
+class Solution(object):
+    def fourSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        ans = []
+        N = len(nums)
+
+        nums.sort()
+        for i in range(N):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            for j in range(i+1, N):
+                if j > i + 1 and nums[j] == nums[j-1]:
+                    continue 
+                
+                k = j + 1
+                l = N - 1
+
+                while k < l:
+                    sum = nums[i] + nums[j] + nums[k] + nums[l]
+                    if sum == target:
+                        temp = [nums[i], nums[j], nums[k], nums[l]]
+                        ans.append(temp)
+
+                        k += 1
+                        l -= 1
+                        while k < l and nums[k] == nums[k-1]:
+                            k += 1
+                        while k < l and nums[l] == nums[l+1]:
+                            l -= 1
+                    elif sum < target:
+                        k += 1
+                    else:
+                        l -= 1
+        return ans
