71549a1966f89f19e3735efe2276f8457cba7c05
Sravanth Kumar Yadav <sravanthkt.21@gmail.com>
2025-06-11 22:56:08 +0530

    Time: 22 ms (21.21%), Space: 22.7 MB (26.02%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,8 +1,17 @@
 class Solution:
+    #Brute Force Solution TC - O(m * n logn) and SC - (m * n)
+    # def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+    #     res = defaultdict(list)
+    #     for s in strs:
+    #         sortedS = ''.join(sorted(s))
+    #         res[sortedS].append(s)
+    #     return list(res.values())
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         res = defaultdict(list)
         for s in strs:
-            sortedS = ''.join(sorted(s))
-            res[sortedS].append(s)
+            count = [0] * 26 
+            for c in s:
+                count[ord(c) - ord('a')] += 1
+            res[tuple(count)].append(s)
         return list(res.values())
-        
+        #Optimal Approach using TC - O(m * n) and SC - O(m * n)
