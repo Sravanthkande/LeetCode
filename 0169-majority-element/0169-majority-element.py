e38d17bbd00be60ee40158cab760e4e47f562ee9
Sravanth Kumar Yadav <sravanthkt.21@gmail.com>
2025-06-11 23:23:01 +0530

    Time: 7 ms (57.93%), Space: 19.2 MB (87.28%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,7 +1,18 @@
 class Solution:
+    #Brute Force approach using HashMap TC-O(n) and SC-O(n)
+    # def majorityElement(self, nums: List[int]) -> int:
+    #     count = defaultdict(int)
+    #     candidate, maxCount = 0, 0
+
+    #     for num in nums:
+    #         count[num] += 1
+    #         if maxCount < count[num]:
+    #             candidate = num
+    #             maxCount = count[num]
+    #     return candidate
     def majorityElement(self, nums: List[int]) -> int:
-        candidate = None
-        count = 0
+        #Optimal Approach Using Moore Vooting Algo TC-O(n) and SC-O(1)
+        count, candidate = 0, None
 
         for num in nums:
             if count == 0:
@@ -11,7 +22,8 @@ class Solution:
                 count += 1
             else:
                 count -= 1
-            
+        
         if nums.count(candidate) > len(nums) // 2:
             return candidate
         return None
+        
