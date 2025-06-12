fb73a63c5cfa07d588e92127ead03e9e4b8fffd5
Sravanth Kumar Yadav <sravanthkt.21@gmail.com>
2025-06-12 22:46:43 +0530

    Time: 0 ms (100%), Space: 23.2 MB (16.05%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -1,4 +1,10 @@
 class Solution:
+    # for i in range(len(nums)):
+    #         for j in range(i, len(nums)):
+    #             if nums[i] > nums[j]:
+    #                 nums[i], nums[j] = nums[j], nums[i]
+            
+    #     return nums, Brute force approach using TC-O(N) and SC-O(N)
     def reverseString(self, s: List[str]) -> None:
         """
         Do not return anything, modify s in-place instead.
@@ -9,3 +15,4 @@ class Solution:
             s[left], s[right] = s[right], s[left]
             left += 1
             right -= 1
+        #optimal approach using TC-O(N) and SC-O(N)
