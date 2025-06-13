3db692aefc7e544f4494b13adc992df35b342776
Sravanth Kumar Yadav <sravanthkt.21@gmail.com>
2025-06-13 14:46:00 +0530

    Time: 0 ms (100%), Space: 18.7 MB (73.87%) - LeetHub

--- 0792-binary-search/0792-binary-search.py
+++ 0792-binary-search/0792-binary-search.py
@@ -1,14 +1,20 @@
 class Solution:
+    # def search(self, nums: List[int], target: int) -> int:
+    #     for i in range(len(nums)):
+    #         if nums[i] == target:
+    #             return i 
+    #     return -1 TC-O(N) sc- O(1) using Linear Search
+
     def search(self, nums: List[int], target: int) -> int:
-        N = len(nums)
-        left, right = 0, N -1
+        left, right = 0, len(nums) -1
 
         while left <= right:
-            mid = (left + right) //2
+            mid = (left + right) // 2
+
             if nums[mid] == target:
-                return mid
+                return mid 
             elif nums[mid] < target:
                 left = mid + 1
             else:
                 right = mid - 1
-        return -1
+        return -1 #This is binary search using O(log n) and O(1)
