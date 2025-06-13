ecdfcecf66a92ceb53f3f5614b5d4954a24c6f7d
Sravanth Kumar Yadav <sravanthkt.21@gmail.com>
2025-06-13 14:56:51 +0530

    Time: 0 ms (100%), Space: 18.5 MB (43.9%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.py
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -1,14 +1,24 @@
 class Solution:
+    # def searchInsert(self, nums: List[int], target: int) -> int:
+    #     N = len(nums)
+    #     for i in range(N):
+    #         if nums[i] >= target:
+    #             return i
+    #     return N tc-O(n), sc-O(1) using linear search
+
     def searchInsert(self, nums: List[int], target: int) -> int:
-        N = len(nums)
-        ans = N
-        left, right = 0, N-1
+        left, right = 0, len(nums) - 1
+        res = len(nums)
 
         while left <= right:
             mid = (left + right) // 2
-            if nums[mid] >= target:
-                ans = mid
+            if nums[mid] == target:
+                return mid
+            if nums[mid] > target:
+                res = mid
                 right = mid - 1
             else:
                 left = mid + 1
-        return ans
+        return res #Using binary search tc-O(logn) and sc-O(1)
+
+            
