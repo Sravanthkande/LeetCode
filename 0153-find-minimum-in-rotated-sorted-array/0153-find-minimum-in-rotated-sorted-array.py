104e64ef4d93d9072d2ff6c4d6f8b259b13b0230
Sravanth Kumar Yadav <sravanthkt.21@gmail.com>
2025-06-17 19:15:39 +0530

    Time: 0 ms (100%), Space: 18.1 MB (31.89%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
@@ -1,16 +1,24 @@
 class Solution:
+    # def findMin(self, nums: List[int]) -> int:
+    #     minEle = float('inf')
+
+    #     for num in nums:
+    #         if num < minEle:
+    #             minEle = min(minEle, num)
+            
+    #     return minEle this is the brute force approach using TC-O(N) and SC-O(1)
     def findMin(self, nums: List[int]) -> int:
         N = len(nums)
-        left, right = 0, N-1
-        mini = float('inf')
-
-        while left <= right:
-            mid = (left + right) // 2
-            if nums[left] <= nums[mid]:
-                mini = min(mini, nums[left])
-                left = mid + 1
-            else:
-                mini = min(mini, nums[mid])
-                right = mid - 1
-        return mini
         
+        left, right = 0, N -1 
+
+        while left < right:
+            mid = (left + right) // 2
+            if nums[mid] <= nums[right]:
+                right = mid 
+            else:
+                left = mid + 1
+            
+
+        return nums[left]
+        #this is the optimal appraoch using binary search TC-O(logn) and SC-O(1)
