a52d2174a5e01e620f47ccefd96d4fea6949a83a
Sravanth Kumar Yadav <sravanthkt.21@gmail.com>
2025-06-19 11:15:58 +0530

    Time: 3 ms (88%), Space: 18.8 MB (98.5%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,38 +1,73 @@
-from collections import deque 
-
 class Solution:
-    def kahnsAlgo(self, N, adj):
-        inDegree = [0] * N 
-        ans = []
+    # def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+    #     #make a graph from prerequisites
 
-        for i in range(N):
-            for neighbour in adj[i]:
-                inDegree[neighbour] += 1
+    #     graph = defaultdict(list)
 
+    #     for u, v in prerequisites:
+    #         graph[u].append(v)
+        
+    #     vis = [0] * numCourses
+
+    #     def dfs(node):
+
+    #         if vis[node] == 1:
+    #             return False 
+            
+    #         if vis[node] == 2:
+    #             return True 
+            
+    #         vis[node] = 1
+
+    #         for neighbour in graph[node]:
+    #             if not dfs(neighbour):
+    #                 return False 
+                
+    #         vis[node] = 2
+    #         return True 
+        
+    #     for i in range(numCourses):
+    #         if not dfs(i):
+    #             return False 
+            
+    #     return True this is a DFS approach using TC - O(v + e) and SC -O(v + e)
+
+    def kahnsAlgo(self, v, graph):
+        degree = [0] * v
+
+        for i in range(v):
+            for neighbour in graph[i]:
+                degree[neighbour] += 1
+            
         que = deque()
 
-        for i in range(N):
-            if inDegree[i] == 0:
+        for i in range(v):
+            if degree[i] == 0:
                 que.append(i)
             
+        ans = []
+
         while que:
             node = que.popleft()
             ans.append(node)
-            for neighbour in adj[node]:
-                inDegree[neighbour] -= 1
 
-                if inDegree[neighbour] == 0:
+            for neighbour in graph[node]:
+                degree[neighbour] -= 1
+                
+                if degree[neighbour] == 0:
                     que.append(neighbour)
-        return ans 
-
+                
+        return ans
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        adj = [[] for _ in range(numCourses)]
+        graph = [[] for _ in range(numCourses)]
 
         for u, v in prerequisites:
-            adj[v].append(u)
+            graph[v].append(u)
         
-        topo = self.kahnsAlgo(numCourses, adj)
+        topo = self.kahnsAlgo(numCourses, graph)
 
         if len(topo) < numCourses:
-            return False
+            return False 
         return True
+        #This approach using topo sort algo TC-O(v + e) and SC-O(v + e)
+        
