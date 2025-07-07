5a63ed048df426d5cd58eb7d255934fa575bd685
Sravanth Kumar Yadav <sravanthkt.21@gmail.com>
2025-07-08 00:10:44 +0530

    Time: 87 ms (28.82%), Space: 22.8 MB (41.66%) - LeetHub

--- 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.py
+++ 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.py
@@ -1,4 +1,23 @@
 class Solution:
+    # def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+    #     points.sort(key = lambda p: p[0] ** 2 + p[1] ** 2)
+    #     return points[:k]  this is brute force approach using sorting algo
+    # Time complexity: O(n log n) and SC: O(1)
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        points.sort(key = lambda p: p[0] ** 2 + p[1] ** 2)
-        return points[:k]
+        minHeap = []
+
+        for x, y in points:
+            dist = (x ** 2) + (y ** 2)
+            minHeap.append([dist, x, y])
+        
+        heapq.heapify(minHeap)
+        res = []
+
+        while k > 0:
+            dist, x, y = heapq.heappop(minHeap)
+            res.append([x,y])
+            k -= 1
+
+        return res
+        #this is the optimal approach using Heapify Algorithm 
+        #tc : O(k * log n) and sc:O(n)
