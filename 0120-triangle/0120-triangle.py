664116dc280ecb7ffa76d9ddd9cb0efb27ef044c
Sravanth Kumar Yadav <sravanthkt.21@gmail.com>
2025-07-11 19:41:36 +0530

    Time: 3 ms (85.8%), Space: 18.7 MB (91.4%) - LeetHub

--- 0120-triangle/0120-triangle.py
+++ 0120-triangle/0120-triangle.py
@@ -1,19 +1,55 @@
 class Solution:
-    def func(self, i, j, grid, N, dp):
-        if i == N - 1:
-            return grid[i][j]
+    # def func(self, i, j, grid, N, dp):
+    #     if i == N - 1:
+    #         return grid[i][j]
         
-        if dp[i][j] != -1:
-            return dp[i][j]
+    #     if dp[i][j] != -1:
+    #         return dp[i][j]
         
-        down = grid[i][j] + self.func(i + 1, j, grid, N, dp)
-        diagonal = grid[i][j] + self.func(i + 1, j + 1, grid, N, dp)
+    #     down = grid[i][j] + self.func(i + 1, j, grid, N, dp)
+    #     diagonal = grid[i][j] + self.func(i + 1, j + 1, grid, N, dp)
 
-        dp[i][j] = min(down, diagonal)
-        return dp[i][j]
+    #     dp[i][j] = min(down, diagonal)
+    #     return dp[i][j] this is memoization approach using TC-O(N), SC-O(N) + O(N * N)
+
+    # def func(self, i, j, grid, N, dp):
+    #     for j in range(N):
+    #         dp[N-1][j] = grid[N-1][j]
+        
+    #     for i in range(N -2, -1, -1):
+    #         for j in range(i + 1):
+
+    #             down = grid[i][j] + dp[i+1][j]
+    #             diagonal = grid[i][j] + dp[i+1][j+1]
+
+    #             dp[i][j] = min(down, diagonal)
+            
+    #     return dp[0][0]
+    
+    #this is tabulation approach using TC-O(N*N), SC-O(N*N)
+
+    def func(self, grid, N):
+        front = [0] * N 
+        cur = [0] * N
+
+        for j in range(N):
+            front[j] = grid[N-1][j]
+        
+        for i in range(N-2, -1, -1):
+            for j in range(i + 1):
+
+                down = grid[i][j] + front[j]
+                diagonal = grid[i][j] + front[j + 1]
+
+                cur[j] = min(down, diagonal)
+            
+            front = cur[:]
+        
+        return front[0]
+
+    #This is optimized approach using TC-O(N * N), SC-O(N) + O(N)
 
     def minimumTotal(self, triangle: List[List[int]]) -> int:
         N = len(triangle)
 
-        dp = [[-1] * N for _ in range(N)]
-        return self.func(0, 0, triangle, N, dp)
+        return self.func(triangle, N)
