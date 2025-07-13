4df51dd6622802402e16ba11040f2d9100aa1bc5
Sravanth Kumar Yadav <sravanthkt.21@gmail.com>
2025-07-13 07:31:40 +0530

    Time: 11 ms (9.71%), Space: 21.2 MB (6.91%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,24 +1,17 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         N = len(prices)
-        def dfs(ind, buy, dp):
-            if ind == N:
-                return 0
-            
-            if dp[ind][buy] != -1:
-                return dp[ind][buy]
-            
-            if buy:
-                buyToday = -prices[ind] + dfs(ind+1, 0, dp)
-                skip = dfs(ind + 1,  1, dp)
-                dp[ind][buy] = max(buyToday, skip)
-            
-            else:
-                sellToday = prices[ind] + dfs(ind + 1, 1, dp)
-                skip = dfs(ind + 1,  0, dp)
-                dp[ind][buy] = max(sellToday, skip)
-            
-            return dp[ind][buy]
-        
-        dp = [[-1] * 2 for _ in range(N)]
-        return dfs(0, 1, dp)
+        dp = [[0] * 2 for _ in range(N + 1)]
+
+        for ind in range(N - 1, -1, -1):
+            for buy in range(2):
+                if buy:
+                    buyToday = -prices[ind] + dp[ind + 1][0]
+                    skip = dp[ind + 1][1]
+                    dp[ind][buy] = max(buyToday, skip)
+                else:
+                    sellToday = prices[ind] + dp[ind + 1][1]
+                    skip = dp[ind + 1][0]
+                    dp[ind][buy] = max(sellToday, skip)
+                
+        return dp[0][1]
